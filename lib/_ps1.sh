3c1f1d1c9ffedc054d65ad3a0e00c217dda1e919
Robin Radic <robin@radic.nl>
2016-09-17 13:16:35 +0200

    upd

---
+++ lib/_ps1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mydir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+source "$mydir/ps1/ps1.sh"
---
+++ lib/ps1/ps1.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+rdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+#source $rdir/config.sh
+source $rdir/liquidprompt.sh
+
+#source $rdir/theme.sh
+#source $rdir/template.sh
